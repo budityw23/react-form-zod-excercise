6fef8f9bdb24a74e087ba99a203387c0f3add320
budi.triwibowo <budi.triwibowo@kuelap.io>
2024-03-24 12:06:37 +0700

    add statrter

---
+++ app/components/FormField.tsx
@@ -0,0 +1,5 @@
+function FormField() {
+  return <></>;
+}
+
+export default FormField;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hello!</h1>
+    </main>
+  );
+}
+
+export default Home;
